621353400aa0fd9b957a87c48c502f9c547a38c4
M Nabeel Arshad <nabeel_arshad93@outlook.com>
2024-11-29 18:27:05 +0500

    gam updated

--- app/Views/site/compaign/play.php
+++ app/Views/site/compaign/play.php
@@ -1,14 +1,6 @@
-<?php print view('site/header');?>
+
 <?php if(getUserId() > 0){ ?>
 	<div class="embed-responsive embed-responsive-1by1">
-		<iframe style="width:99vw !important;height:100vh !important;" class="embed-responsive-item" src="<?=base_url()?>spin-v-1.0.0/index.html" ></iframe>
+		<iframe style="width:99vw !important;height:100vh !important;" class="embed-responsive-item" src="<?=base_url()?>spin-v-1.0.01/index.html" ></iframe>
 	</div>
-<?php } ?>
-<?php print view('site/footer');?>
-<?php if(!getUserId()){ ?>
-	<script>
-		$(document).ready(function(){
-			$('.btnlogin').click();
-		})
-	</script>
 <?php } ?>
