1b0f34075fba81ad370ca6eccff385a317c45bb6
M Nabeel Arshad <nabeel_arshad93@outlook.com>
2024-12-01 00:14:01 +0500

    new game build added

--- app/Views/site/compaign/play.php
+++ app/Views/site/compaign/play.php
@@ -1,7 +1,7 @@
 
 <?php if(getUserId() > 0){ ?>
 	<div class="embed-responsive embed-responsive-1by1">
-		<iframe style="width:99vw !important;height:100vh !important;" class="embed-responsive-item" src="<?=base_url()?>spin-v-1.0.01/index.html" ></iframe>
+		<iframe style="width:99vw !important;height:100vh !important;" class="embed-responsive-item" src="<?=base_url()?>spin-v-1.0.02/index.html" ></iframe>
 	</div>
 <?php }else { ?>
 	<script>
